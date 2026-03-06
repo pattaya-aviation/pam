e78f4695542d3aa1b973fc3ec7906e07632cce64
Pattaya Aviation <hr.developer@pattayaaviation.com>
2026-03-06 15:12:55 +0700

    Delete _trim2.ps1

--- _trim2.ps1
+++
@@ -1,6 +0,0 @@
-$f = "c:\dev\pa-system\page\portal\vfc\index.html"
-$l = Get-Content $f -Encoding UTF8
-# Keep lines 1-90 (indices 0-89) and 149 onwards (index 148+)
-$k = $l[0..89] + $l[148..($l.Count - 1)]
-Set-Content $f $k -Encoding UTF8
-Write-Host "Done. Lines: $($k.Count)"
